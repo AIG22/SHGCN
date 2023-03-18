6c6504d284eaf0f086be5332259b1fff486760a4
22AI <58290928+AIG22@users.noreply.github.com>
2023-03-18 11:27:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-python train.py --dataset Cora --lr 0.4 --weight_decay 5e-4 --dropout 0.4 --dim 8
-python train.py --dataset Citeseer --lr 0.3 --weight_decay 5e-4 --dropout 0.5 --dim 16
-python train.py --dataset Pubmed --lr 0.08 --weight_decay 5e-4 --dropout 0.2 --dim 16
-python train.py --dataset Chameleon --lr 0.07 --weight_decay 5e-6 --dropout 0.8 --dim 8
 python train.py --dataset Squirrel --lr 0.05 --weight_decay 0.0 --dropout 0.7 --dim 32
-python train.py --dataset Actor --lr 0.2 --weight_decay 5e-3 --dropout 0.0 --dim 8
-python train.py --dataset Texas --lr 0.07 --weight_decay 5e-4 --dropout 0.5 --dim 8
-python train.py --dataset Cornell --lr 0.2 --weight_decay 5e-4 --dropout 0.4 --dim 8
